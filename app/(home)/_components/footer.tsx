74d088d74c0c09ecc973cc8c2beeb925c50c049b
akshay-kumar4 <akshaykhigh8285@gmail.com>
2023-12-21 12:48:40 +0530

    added homepage

---
+++ app/(home)/_components/footer.tsx
@@ -0,0 +1,18 @@
+import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+
+const Footer = () => {
+  return (
+    <div className="fixed bottom-0 w-full p-4 border-t bg-white flex items-center">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+          <Button size="sm" variant="ghost">Privacy Policy</Button>
+          <Button size="sm" variant="ghost">Terms & Conditions</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ app/(home)/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./_components/navbar";
+import Footer from "./_components/footer";
+
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-slate-100">
+      <main className="pt-40 pb-20 bg-slate-100">
+        <Navbar />
+        {children}
+        <Footer />
+      </main>
+    </div>
+  );
+};
+
+export default HomeLayout;
